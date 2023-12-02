e243945e3871e6846f0d75640694fde0b22a5ee9
Pang <jonathan.pang@oliverwyman.com>
2023-12-01 23:20:06 -0500

    2023 Day 1 completed

---
+++ 2023/Day-1/trebuchet.py
@@ -0,0 +1,42 @@
+def main():
+    cal_inputs = open("input.txt")
+    print(extract_numerical_digits(cal_inputs))
+    print(extract_any_digits(cal_inputs))
+
+
+def extract_numerical_digits(cal_inputs):
+    cal_inputs.seek(0)
+
+    total_cal_values = 0
+    for cal_input in cal_inputs:
+        cal_digits = list(filter(str.isdigit, cal_input))
+
+        total_cal_values = total_cal_values + int(str(list(cal_digits)[0]) + str(list(cal_digits)[len(cal_digits)-1]))
+    return total_cal_values
+
+
+def extract_any_digits(cal_inputs):
+    cal_inputs.seek(0)
+
+    word_digits = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10}
+    total_cal_values = 0
+
+    for cal_input in cal_inputs:
+        min_word_digit_indices = {cal_input.find(word_digit): word_digit for word_digit in word_digits}
+        min_word_index = min(filter(lambda x: x > -1, min_word_digit_indices.keys()), default=None)
+
+        max_word_digit_indices = {cal_input.rfind(word_digit): word_digit for word_digit in word_digits}
+        max_word_index = max(filter(lambda x: x > -1, max_word_digit_indices.keys()), default=None)
+
+        digit_indices = [x for x, is_digit in enumerate([x.isdigit() for x in cal_input]) if is_digit is True]
+
+        digit_1 = word_digits[min_word_digit_indices[min_word_index]] if min_word_index and (min_word_index < digit_indices[0]) else cal_input[digit_indices[0]]
+        digit_2 = word_digits[max_word_digit_indices[max_word_index]] if max_word_index and (max_word_index > digit_indices[len(digit_indices)-1]) else cal_input[digit_indices[len(digit_indices)-1]]
+
+        total_cal_values = total_cal_values + int(str(digit_1) + str(digit_2))
+
+    return total_cal_values
+
+
+if __name__ == "__main__":
+    main()
